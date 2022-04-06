f8cbfdaf38d059cbab0239148dbeb93ad5eb8a4b
Matheus Souza de Almeida <Matheus.Souza.almeida@hotmail.com>
2022-04-06 20:41:00 -0300

    feat: subindo a aba sobre

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname,'views/index.html')))
+app.get('/sobre', (req, res) => res.sendFile(path.join(__dirname,'views/sobre.html')))
 
 app.listen(port, () => console.info(`Servidor rodando na porta ${port}`))
